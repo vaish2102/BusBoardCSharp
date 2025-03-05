2e6c06e5cbfd29ff7750fa48b254649f3a64c4d7
Vaishnavi Rajasekar <vaishnavi.rajasekar@softwire.com>
2025-03-05 10:12:31 +0000

    Deleted unwanted files

--- BusBoardCSharp/AppSettings.cs
+++ BusBoardCSharp/AppSettings.cs
@@ -3,10 +3,11 @@ using Microsoft.Extensions.Configuration;
 namespace BusBoardCSharp{
     class AppSettings{
 
-        /*public AppSettings(){
-
-        }*/
-       string Api_Key = "";
+        public string Api_Key;
+        public AppSettings(string api_key){
+            Api_Key = api_key;
+        }
+       //string Api_Key = "";
        public string GetAPIKey(){
            IConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
            configurationBuilder.AddJsonFile(Directory.GetCurrentDirectory()+"\\appsettings.json", false, true);
